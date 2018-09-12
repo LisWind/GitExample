f3b712b444b963bc25dd489fff4913908ab714af
LisWind <kobbey@163.com>
2018-09-12 21:22:19 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,4 +5,6 @@ int main()
 	std::cout << "Hello, World!" << std::endl;
 
 	std::cout << "This time do something new." << std::endl;
+	
+	std::cout << "~~~add something at origin~~~" << std::endl;
 }

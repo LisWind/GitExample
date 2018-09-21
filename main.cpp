0f04a2acfb3e2862062e018803bcc146b577b06b
LisWind <kobbey@163.com>
2018-09-21 09:22:09 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,4 +7,6 @@ int main()
 	std::cout << "This time do something new." << std::endl;
 	
 	std::cout << "~~~add something at origin~~~" << std::endl;
+	
+	std::cout << " test 'local' using 'git stash'. " << std::endl
 }
